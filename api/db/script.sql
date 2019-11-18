7fd230bba7a399102f2d19d156bc50b511d50111
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2019-11-17 22:24:42 -0300

    Criado script sql para o banco de dados

---
+++ api/db/script.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS
+appnodemysqlphp;
+
+USE appnodemysqlphp;
+
+CREATE TABLE IF NOT EXISTS pokemons (
+    id INT(11) AUTO_INCREMENT,
+    name VARCHAR(255),
+    price DECIMAL(10, 2),
+    PRIMARY KEY (id)
+);
+
+INSERT INTO pokemons (0, 'Pikachu', 2000);
+INSERT INTO pokemons (0, 'Psyduck', 1800);
+INSERT INTO pokemons (0, 'Eevee', 1700);
+INSERT INTO pokemons (0, 'Squirtle', 2000);
+INSERT INTO pokemons (0, 'Bulbasauro', 2000);
+INSERT INTO pokemons (0, 'Charmander', 2000);
